1.4.0a9 addding in mcmc gauss fit fix error in reading in the rsp output (wrong units 10^-18 instead of 10^-17 on flux) starting to remove 'sbr' (spectrum::est_peak_strength) make checking catalog overlap simpler (--catcheck)

Bayes/bayesian.py
@@ -104,18 +104,31 @@ def prob_ratio(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,addl_wavelen
 
 
 
-def prob_data_given_LAE(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,addl_wavelengths,sky_area,cosmo,LAE_priors):
+def prob_data_given_LAE(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,addl_wavelengths,sky_area,cosmo,
+                        LAE_priors,wl_obs_hilo=None,lineFlux_hilo=None,ew_obs_hilo=None):
    t0 = time.time()
    
    z_LAE = wl_obs/1215.668-1
-   L = 4*np.pi*( 3.08567758e24 * nb.lumDist(z_LAE,cosmo) )**2 * lineFlux
+   # DD: 2018-07-25 reworked this section to use uncertainties
+   prob_EW = 1. # DD: can be not set ... so this is a safety
+
+   L_base = 4*np.pi*( 3.08567758e24 * nb.lumDist(z_LAE,cosmo) )**2
+
+   if lineFlux_hilo is not None:
+      L_hi = L_base * lineFlux_hilo[0]
+      L_lo = L_base * lineFlux_hilo[1]
+   else:
+      L_hi = L_base * lineFlux * 1.05
+      L_lo = L_base * lineFlux * 0.95
+
+   #L = 4*np.pi*( 3.08567758e24 * nb.lumDist(z_LAE,cosmo) )**2 * lineFlux
    norm_to_1, phiStar_LAE, LStar, z_LAE, L_min = LAE_LF(wl_obs,sky_area,LAE_priors,cosmo)
    ### third argument '1' denotes Ci12, evolving LF between z = 2.1 and z = 3.1
    
    LStar = LStar * mult_LStar_LAE
    phiStar_LAE = phiStar_LAE * mult_phiStar_LAE
    
-   prob_lineFlux = norm_to_1 * phiStar_LAE * ( float(mpm.gammainc(alpha_LAE+1,0.95*L/LStar)) - float(mpm.gammainc(alpha_LAE+1,1.05*L/LStar)) )
+   prob_lineFlux = norm_to_1 * phiStar_LAE * ( float(mpm.gammainc(alpha_LAE+1,L_lo/LStar)) - float(mpm.gammainc(alpha_LAE+1,L_hi/LStar)) )
 
    ### same thing; chain rule + recusive definition of gamma function (15-08-17)
    #prob_lineFlux = norm_to_1 * phiStar_LAE / (alpha_LAE+1) * ( float(mpm.gammainc(alpha_LAE+2,0.95*L/LStar)-(0.95*L/LStar)**(alpha_LAE+1)*np.exp(-0.95*L/LStar)) - float(mpm.gammainc(alpha_LAE+2,1.05*L/LStar)-(1.05*L/LStar)**(alpha_LAE+1)*np.exp(-1.05*L/LStar)) )
@@ -128,20 +141,27 @@ def prob_data_given_LAE(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,add
       need to use recursion relation for the incomplete gamma function to integral under the schechter function
       
    '''
-
-   if not which_color == 'no_imaging':
-      
-      w_0 = LAE_EW(wl_obs,LAE_priors)
+   # DD: 2018-07-25 reworked this section to use uncertainties
+   if not (which_color == 'no_imaging') and (ew_obs > 0):
+      #if ew_obs <= 0:         ### (10/14/14) allow negative EW (only LAEs affected because [O II]s are bright in the continuum)
+      #   prob_EW = 1.
+      #
+      # else:
+      w_0 = LAE_EW(wl_obs, LAE_priors)
       ### last argument '1' denotes Ci12, evolving LF between z = 2.1 and z = 3.1
 
       w_0 = w_0 * mult_w0_LAE
-      ew_rest = ew_obs/(1+z_LAE)
+      # ew_rest = ew_obs/(1+z_LAE)
 
-      if ew_obs <= 0:         ### (10/14/14) allow negative EW (only LAEs affected because [O II]s are bright in the continuum)
-         prob_EW = 1.
-      
+      if ew_obs_hilo is not None:
+         ew_rest_hi = ew_obs_hilo[0] / (1. + z_LAE)
+         ew_rest_lo = ew_obs_hilo[1] / (1. + z_LAE)
       else:
-         prob_EW = -exp(-1.05*ew_rest/w_0) - ( -exp(-0.95*ew_rest/w_0) )
+         ew_rest_hi = ew_obs * 1.05 / (1. + z_LAE)
+         ew_rest_lo = ew_obs * 0.95 / (1. + z_LAE)
+
+      #prob_EW = -exp(-1.05*ew_rest/w_0) - ( -exp(-0.95*ew_rest/w_0) )
+      prob_EW = -exp(-ew_rest_hi / w_0) - (-exp(-ew_rest_lo/ w_0))
       
    ###### (04-24-15)
    addl_em_lines  = ['[NeIII]','H_beta','[OIII]','[OIII]']
@@ -219,11 +239,26 @@ def prob_data_given_LAE(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,add
 
 
 
-def prob_data_given_OII(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,addl_wavelengths,sky_area,cosmo,EW_case,z_OII_list,W_0_list,sigma_list):
+def prob_data_given_OII(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,addl_wavelengths,sky_area,cosmo,
+                        EW_case,z_OII_list,W_0_list,sigma_list,wl_obs_hilo=None,lineFlux_hilo=None,ew_obs_hilo=None):
+   #DD 2018-07-25 the *_hilo are uncertainties, expected to be a 2 element array or tuple with the
+   # high then low values (e.g. with uncertainties added in, NOT the uncertainties themselves, so 10 +/-5 is passed
+   # as (15,5) or [15,5]
+
    t0 = time.time()
+   prob_EW = 1.  # DD: can be not set ... so this is a safety
    
    z_OII = wl_obs/3727.45-1
-   L = 4*np.pi*( 3.08567758e24 * nb.lumDist(z_OII,cosmo) )**2 * lineFlux
+   # DD: 2018-07-25 reworked this section to use uncertainties
+   L_base = 4 * np.pi * (3.08567758e24 * nb.lumDist(z_OII, cosmo)) ** 2
+
+   if lineFlux_hilo is not None:
+      L_hi = L_base * lineFlux_hilo[0]
+      L_lo = L_base * lineFlux_hilo[1]
+   else:
+      L_hi = L_base * lineFlux * 1.05
+      L_lo = L_base * lineFlux * 0.95
+
    norm_to_1, phiStar_OII, LStar, z_OII, L_min = OII_LF(wl_obs,sky_area,cosmo)
    
    LStar = LStar * mult_LStar_OII
@@ -232,7 +267,7 @@ def prob_data_given_OII(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,add
    
    if wl_obs < 3727.45: prob_lineFlux = 0         ## wavelength observed shorter than [O II] rest frame
    else:
-      prob_lineFlux = norm_to_1 * phiStar_OII * ( float(mpm.gammainc(alpha_OII+1,0.95*L/LStar)) - float(mpm.gammainc(alpha_OII+1,1.05*L/LStar)) )
+      prob_lineFlux = norm_to_1 * phiStar_OII * ( float(mpm.gammainc(alpha_OII+1,L_lo/LStar)) - float(mpm.gammainc(alpha_OII+1,L_hi/LStar)) )
       ### same thing; chain rule + recusive definition of gamma function (15-08-17)
       #prob_lineFlux = norm_to_1 * phiStar_OII / (alpha_OII+1) * ( float(mpm.gammainc(alpha_OII+2,0.95*L/LStar)-(0.95*L/LStar)**(alpha_OII+1)*np.exp(-0.95*L/LStar)) - float(mpm.gammainc(alpha_OII+2,1.05*L/LStar)-(1.05*L/LStar)**(alpha_OII+1)*np.exp(-1.05*L/LStar)) )
       
@@ -245,31 +280,42 @@ def prob_data_given_OII(wl_obs,lineFlux,ew_obs,c_obs,which_color,addl_fluxes,add
       
    '''
 
-   if not which_color == 'no_imaging':
+   #if not which_color == 'no_imaging':
+   #DD: 2018-07-25 reworked this section to use uncertainties
+   if not (which_color == 'no_imaging') and (ew_obs > 0):
 
-      ew_rest = ew_obs/(1+z_OII)
-      
-      if ew_obs <= 0:         ### (10/14/14) allow negative EW (only LAEs affected because [O II]s are bright in the continuum)
-         prob_EW = 0.
-      
+      ew_rest_mean = ew_obs/(1+z_OII)
+
+      if ew_obs_hilo is not None:
+         ew_rest_hi = ew_obs_hilo[0] / (1. + z_OII)
+         ew_rest_lo = ew_obs_hilo[1] / (1. + z_OII)
       else:
-         if (EW_case == 'lognormal' and ew_rest <= 10) or EW_case == 'fully_lognormal':
-            ### (09/03/15) re-fitting for lognormal parameters makes for a good slow-down!
-            ###            0.53 second per object to run prob_ratio() this way
-            ###            0.0096 second per object by pre-fitting lognormal in nb.prob_ratio()
-            ###            0.0036 second per object on grad box
-            #fitParam, fitCovar = fln.run(z_OII,'base')
-            #W_0, sigma = fitParam[0], fitParam[1]
-            
-            W_0   = np.interp(z_OII,z_OII_list,W_0_list)
-            sigma = np.interp(z_OII,z_OII_list,sigma_list)
-            prob_EW = 0.5 * ( ssp.erf((np.log((1.05*ew_rest)/W_0)-0.5*sigma**2)/(np.sqrt(2)*sigma)) - ssp.erf((np.log((0.95*ew_rest)/W_0)-0.5*sigma**2)/(np.sqrt(2)*sigma)) )
+         ew_rest_hi = ew_obs * 1.05 / (1. + z_OII)
+         ew_rest_lo = ew_obs * 0.95 / (1. + z_OII)
       
-         else:
-            w_0 = OII_EW(wl_obs)
-            w_0 = w_0 * mult_w0_OII
-            prob_EW = -np.exp(-1.05*ew_rest/w_0) - ( -np.exp(-0.95*ew_rest/w_0) )
-   
+      #if ew_obs <= 0:         ### (10/14/14) allow negative EW (only LAEs affected because [O II]s are bright in the continuum)
+      #   prob_EW = 0.
+      #
+      #else:
+      if (EW_case == 'lognormal' and ew_rest_mean <= 10) or EW_case == 'fully_lognormal':
+         ### (09/03/15) re-fitting for lognormal parameters makes for a good slow-down!
+         ###            0.53 second per object to run prob_ratio() this way
+         ###            0.0096 second per object by pre-fitting lognormal in nb.prob_ratio()
+         ###            0.0036 second per object on grad box
+         #fitParam, fitCovar = fln.run(z_OII,'base')
+         #W_0, sigma = fitParam[0], fitParam[1]
+
+         W_0   = np.interp(z_OII,z_OII_list,W_0_list)
+         sigma = np.interp(z_OII,z_OII_list,sigma_list)
+         prob_EW = 0.5 * ( ssp.erf((np.log((ew_rest_hi)/W_0)-0.5*sigma**2)/(np.sqrt(2)*sigma)) -
+                           ssp.erf((np.log((ew_rest_lo)/W_0)-0.5*sigma**2)/(np.sqrt(2)*sigma)) )
+
+      else:
+         w_0 = OII_EW(wl_obs)
+         w_0 = w_0 * mult_w0_OII
+         prob_EW = -np.exp(-ew_rest_hi/w_0) - ( -np.exp(-ew_rest_lo/w_0) )
+
+
    ###### (04-24-15)
    addl_em_lines  = ['[NeIII]','H_beta','[OIII]','[OIII]']
    addl_lambda_rf = np.array([3869.00, 4861.32, 4958.91, 5006.84])

exp_prob.py
@@ -1,4 +1,9 @@
 #explore probabilities
+
+RUN = True #if false, reads in the file, if True runs samples and makes a new file
+FILE = "/home/dustin/code/python/voltron/exp_gauss_out.txt"
+SAMPLES = 10000
+
 import numpy as np
 import scipy as sp
 import imp
@@ -68,7 +73,7 @@ def reload():
 class GaussFit:
 
     #all class vars
-    num_trials = 100000 #start with 1000 just to test
+    num_trials = SAMPLES #start with 1000 just to test
     #note: because an entire spectrum is generated and many peaks are evaluated per spectrum
     #      the actualy 'number of trials' is much larger where the meaning is per peak not per spectrum
     num_pix = 1001
@@ -203,7 +208,7 @@ class GaussFit:
                 eli.build(values_units=-18)
                 if eli.line_flux == -999:
                     eli.line_flux = 0.0
-                out.write("%d  %0.2g  %0.2g  %0.2g  %0.2g  %0.2g  %0.2g  %0.2g\n"
+                out.write("%d  %f  %f  %f  %f  %f  %f  %f\n"
                       % (eli.is_good(), eli.snr, eli.fit_sigma, eli.line_flux, eli.cont , eli.sbr, eli.eqw_obs, eli.line_score))
 
 
@@ -220,7 +225,7 @@ class GaussFit:
         eli = []
 
         #sort of a dumb way to do this, but convenient to put in the ELI objects
-        out =  np.loadtxt(filename)
+        out =  np.loadtxt(filename,dtype=np.float64)
         score = out[:,7]
         line_flux = out[:,3]
         snr = out[:,1]
@@ -244,13 +249,12 @@ class GaussFit:
 
 
 def main():
-    RUN = True
     gf = GaussFit()
 
     if (RUN):
         score, line_flux, snr, good, eli = gf.go()
     else:
-        score, line_flux, snr, good, eli = gf.load("~/code/python/voltron/exp_gauss_out.txt")
+        score, line_flux, snr, good, eli = gf.load(FILE)
 
 
     all_score = np.histogram(score, bins=gf.score_bin_edges)
@@ -329,12 +333,20 @@ def main():
     plt.hist([ob.line_flux for ob in eli[np.where(good)[0]]],bins=bins)  # just the good ones
     plt.show()
 
-    # look at raw_score
+
+    # look at line_flux
     plt.close()
-    plt.title=("raw_score")
-    vals, bins, _ =plt.hist([ob.raw_score for ob in eli],bins=50)  # all
-    plt.hist([ob.raw_score for ob in eli[np.where(good)[0]]],bins=bins)  # just the good ones
+    plt.title("DX0")
+    vals, bins, _ =plt.hist(np.clip([ob.fit_dx0 for ob in eli],0,np.inf),bins=50)  # all
+    plt.hist([ob.line_flux for ob in eli[np.where(good)[0]]],bins=bins)  # just the good ones
     plt.show()
+
+    # look at raw_score
+    # plt.close()
+    # plt.title=("raw_score")
+    # vals, bins, _ =plt.hist([ob.raw_score for ob in eli],bins=50)  # all
+    # plt.hist([ob.raw_score for ob in eli[np.where(good)[0]]],bins=bins)  # just the good ones
+    # plt.show()
 
 
 
@@ -342,28 +354,36 @@ def main():
 
     #SNR vs SBR (there isn't one)
     plt.close()
-    plt.title = ("SNR vs SBR")
+    plt.title("SNR vs SBR")
+    plt.xlabel("SNR")
+    plt.ylabel("SBR")
     plt.scatter([ob.snr for ob in eli], [ob.sbr for ob in eli])
     plt.show()
 
 
     #SNR vs sigma (there isn't one)
     plt.close()
-    plt.title = ("SNR vs Sigma")
+    plt.title("SNR vs Sigma")
+    plt.xlabel("SNR")
+    plt.ylabel("sigma")
     plt.scatter([ob.snr for ob in eli], [ob.fit_sigma for ob in eli])
     plt.show()
 
 
     #SNR vs sigma (a little, weak)
     plt.close()
-    plt.title = ("SNR vs EW")
+    plt.title("SNR vs EW")
+    plt.xlabel("SNR")
+    plt.ylabel("EW")
     plt.scatter([ob.snr for ob in eli], [ob.eqw_obs for ob in eli])
     plt.show()
 
 
     #SNR vs line flux (a little, weak)
     plt.close()
-    plt.title = ("SNR vs line_flux")
+    plt.title("SNR vs line_flux")
+    plt.xlabel("SNR")
+    plt.ylabel("Line Flux")
     plt.scatter(np.array([ob.snr for ob in eli]), np.array([ob.line_flux for ob in eli])*1e18)
     plt.show()
 
@@ -371,8 +391,26 @@ def main():
 
     #SNR vs line flux (a little, weak)
     plt.close()
-    plt.title = ("SNR vs line_score")
+    plt.title("SNR vs line_score")
+    plt.xlabel("SNR")
+    plt.ylabel("Line Score")
     plt.scatter(np.array([ob.snr for ob in eli]), np.array([ob.line_score for ob in eli]))
+    plt.show()
+
+
+    plt.close()
+    plt.title("EW vs sigma")
+    plt.xlabel("EW")
+    plt.ylabel("sigma")
+    plt.scatter(np.array([ob.eqw_obs for ob in eli]), np.array([ob.fit_sigma for ob in eli]))
+    plt.show()
+
+
+    plt.close()
+    plt.title("EW vs Line Flux")
+    plt.xlabel("EW")
+    plt.ylabel("Line Flux")
+    plt.scatter(np.array([ob.eqw_obs for ob in eli]), np.array([ob.line_flux for ob in eli])*1e17)
     plt.show()
 
     #histogram

spectrum.py
@@ -26,7 +26,8 @@ log.setlevel(G.logging.DEBUG)
 
 #these are for the older peak finder (based on direction change)
 MIN_FWHM = 2 #AA (must xlat to pixels)
-MIN_ELI_SNR = 2.0 #bare minium SNR to even remotely consider a signal as real
+MIN_ELI_SNR = 3.0 #bare minium SNR to even remotely consider a signal as real
+MIN_ELI_SIGMA = 1.0 #bare minium (expect this to be more like 2+)
 MIN_HEIGHT = 10
 MIN_DELTA_HEIGHT = 2 #to be a peak, must be at least this high above next adjacent point to the left
 DEFAULT_BACKGROUND = 6.0
@@ -45,9 +46,10 @@ GAUSS_FIT_AA_RANGE = 40.0 #AA to either side of the line center to include in th
 GAUSS_FIT_PIX_ERROR = 2.0 #error (freedom) in pixels: have to allow at least 2 pixels of error
                           # (if aa/pix is small, _AA_ERROR takes over)
 GAUSS_FIT_AA_ERROR = 1.0 #error (freedom) in line center in AA, still considered to be okay
-GAUSS_SNR_SIGMA = 3.0 #check at least these pixels (pix*sigma) to either side of the fit line for SNR
-                      # (larger of this or GAUSS_SNR_NUM_AA)
-GAUSS_SNR_NUM_AA = 5.0 #check at least 4 AA to either side (9 total) of the fit line for SNR in gaussian fit
+GAUSS_SNR_SIGMA = 5.0 #check at least these pixels (pix*sigma) to either side of the fit line for SNR
+                      # (larger of this or GAUSS_SNR_NUM_AA) *note: also capped to a max of 40AA or so (the size of the
+                      # 'cutout' of the signal (i.e. GAUSS_FIT_AA_RANGE)
+GAUSS_SNR_NUM_AA = 5.0 #check at least this num of AA to either side (2x +1 total) of the fit line for SNR in gaussian fit
                        # (larger of this or GAUSS_SNR_SIGMA
 
 
@@ -55,27 +57,24 @@ GAUSS_SNR_NUM_AA = 5.0 #check at least 4 AA to either side (9 total) of the fit 
 #if change the noise model or SNR or line_flux algorithm or "EmissionLineInfo::is_good", need to recompute these
 #as MDF
 PROB_NOISE_LINE_SCORE = \
-[  6.5,   7.5,   8.5,   9.5,  10.5,  11.5,  12.5,  13.5,  14.5, 15.5,
-  16.5,  17.5,  18.5,  19.5,  20.5,  21.5,  22.5,  23.5,  24.5, 25.5,
-  26.5,  27.5,  28.5,  29.5,  30.5,  31.5,  32.5]
+[  2.5,   3.5,   4.5,   5.5,   6.5,   7.5,   8.5,   9.5,  10.5,
+        11.5,  12.5,  13.5,  14.5,  15.5]
 
-PROB_NOISE_GIVEN_SCORE = \
-    [2.51860000e-01, 1.96010000e-01, 1.51290000e-01,
-     1.10050000e-01, 7.84900000e-02, 5.61000000e-02,
-     4.05900000e-02, 2.96600000e-02, 2.21200000e-02,
-     1.52200000e-02, 1.14000000e-02, 8.40000000e-03,
-     5.87000000e-03, 4.78000000e-03, 3.65000000e-03,
-     2.59000000e-03, 2.11000000e-03, 1.66000000e-03,
-     1.38000000e-03, 1.21000000e-03, 9.40000000e-04,
-     5.70000000e-04, 6.80000000e-04, 5.60000000e-04,
-     4.50000000e-04, 2.80000000e-04, 2.20000000e-04]
+#PROB_NOISE_GIVEN_SCORE = np.zeros(len(PROB_NOISE_LINE_SCORE))#\
+PROB_NOISE_GIVEN_SCORE =  \
+[        3.77800000e-01,   2.69600000e-01,   1.66400000e-01,
+         9.05000000e-02,   4.69000000e-02,   2.28000000e-02,
+         1.07000000e-02,   7.30000000e-03,   3.30000000e-03,
+         2.30000000e-03,   1.00000000e-03,   5.00000000e-04,
+         5.00000000e-04,   2.00000000e-04]
 
 PROB_NOISE_TRUNCATED = 0.0002 #all bins after the end of the list get this value
-PROB_NOISE_MIN_SCORE = 6.0 #min score that makes it to the bin list
+PROB_NOISE_MIN_SCORE = 2.0 #min score that makes it to the bin list
 
 
 #beyond an okay fit (see GAUSS_FIT_xxx above) is this a "good" signal
-GOOD_MIN_LINE_SCORE = PROB_NOISE_MIN_SCORE # roughly 25% chance of noise
+GOOD_MIN_LINE_SCORE = 2.0 #lines are added to solution only if 'GOOD' (meaning, minimally more likely real than noise)
+#does not have to be the same as PROB_NOISE_MIN_SCORE, but that generally makes sense
 #GOOD_FULL_SNR = 9.0 #ignore SBR is SNR is above this
 #GOOD_MIN_SNR = 5.0 #bare-minimum; if you change the SNR ranges just above, this will also need to change
 #GOOD_MIN_SBR = 3.0 #signal to "background" noise (looks at peak height vs surrounding peaks) (only for "weak" signals0
@@ -91,7 +90,7 @@ GOOD_MAX_DX0_MULT = 1.75 #3.8 (AA) ... now 1.75 means 1.75 * pixel_size
                     #since this is based on the fit of the extra line AND the line center error of the central line
                     #this is a compound error (assume +/- 2 AA since ~ 1.9AA/pix for each so at most 4 AA here)?
 #GOOD_MIN_H_CONT_RATIO = 1.33 #height of the peak must be at least 33% above the continuum fit level
-ADDL_LINE_SCORE_BONUS = 10.0 #add for each line at 2+ lines (so 1st line adds nothing)
+ADDL_LINE_SCORE_BONUS = 5.0 #add for each line at 2+ lines (so 1st line adds nothing)
                             #this is rather "hand-wavy" but gives a nod to having more lines beyond just their score
 #todo: impose line ratios?
 #todo:  that is, if line_x is assumed and line_y is assumed, can only be valid if line_x/line_y ~ ratio??
@@ -242,6 +241,7 @@ class EmissionLineInfo:
         self.fit_vals = []
 
         self.pix_size = None
+        self.sn_pix = 0 #total number of pixels used to calcualte the SN (kind of like a width in pixels)
 
         #!! Important: raw_wave, etc is NOT of the same scale or length of fit_wave, etc
         self.raw_wave = []
@@ -272,10 +272,11 @@ class EmissionLineInfo:
 
 
     def build(self,values_units=0):
-        if self.snr > MIN_ELI_SNR:
+        if self.snr > MIN_ELI_SNR and self.fit_sigma > MIN_ELI_SIGMA:
             if self.fit_sigma is not None:
                 self.fwhm = 2.355 * self.fit_sigma  # e.g. 2*sqrt(2*ln(2))* sigma
 
+            unit = 1.0
             if self.fit_x0 is not None:
 
                 if values_units != 0:
@@ -310,7 +311,30 @@ class EmissionLineInfo:
             if self.line_flux and self.cont:
                 self.eqw_obs = self.line_flux / self.cont
 
-            self.line_score = self.snr * self.line_flux * 1.0e17
+            #line_flux is now in erg/s/... the 1e17 scales up to reasonable numbers (easier to deal with)
+            #we are technically penalizing a for large variance, but really this is only to weed out
+            #low and wide fits that are probably not reliable
+
+            #and penalize for large deviation from the highest point and the line (gauss) mean (center) (1.9 ~ pixel size)
+            #self.line_score = self.snr * self.line_flux * 1e17 / (2.5 * self.fit_sigma * (1. + abs(self.fit_dx0/1.9)))
+
+            #penalize for few pixels (translated to angstroms ... anything less than 21 AA total)
+            #penalize for too narrow sigma (anything less than 1 pixel
+
+            #the 10.0 is just to rescale ... could make 1e17 -> 1e16, but I prefer to read it this way
+
+            self.line_score = self.snr * self.line_flux * 1e17 * \
+                              min(self.fit_sigma/self.pix_size,1.0) * \
+                              min((self.pix_size * self.sn_pix)/21.0,1) / \
+                              (10.0 * (1. + abs(self.fit_dx0 / 1.9)) )
+            #
+            # !!! if you change this calculation, you need to re-calibrate the prob(Noise) (re-run exp_prob.py)
+            # !!! and update the Solution cut criteria in global_config.py (MULTILINE_MIN_SOLUTION_SCORE, etc) and
+            # !!!    in hetdex.py (DetObj::multiline_solution_score)
+            # !!! and update GOOD_MIN_LINE_SCORE and PROB_NOISE_MIN_SCORE
+            # !!! It is a little ciruclar as MIN scores are dependent on the results of the exp_prob.py run
+            #
+
             self.prob_noise = self.get_prob_noise()
         else:
             self.fwhm = -999
@@ -319,12 +343,11 @@ class EmissionLineInfo:
             self.line_score = 0
 
     def get_prob_noise(self):
-
         MDF = False
 
         try:
             if (self.line_score is None) or (self.line_score < PROB_NOISE_MIN_SCORE):
-                return 1.0 # really not, but we will cap it
+                return 0.98 # really not, but we will cap it
             #if we are off the end of the scores, set to a fixed probability
             elif self.line_score > max(PROB_NOISE_LINE_SCORE) + (PROB_NOISE_LINE_SCORE[1]-PROB_NOISE_LINE_SCORE[0]):
                 return PROB_NOISE_TRUNCATED #set this as the minium
@@ -359,7 +382,7 @@ class EmissionLineInfo:
         result = False
 
         # minimum to be possibly good
-        if (self.line_score > GOOD_MIN_LINE_SCORE) and (self.fit_sigma >= GOOD_MIN_SIGMA):
+        if (self.line_score >= GOOD_MIN_LINE_SCORE) and (self.fit_sigma >= GOOD_MIN_SIGMA):
             result = True
             #note: GOOD_MAX_DX0_MULT enforced in signal_score
 
@@ -559,6 +582,7 @@ def signal_score(wavelengths,values,errors,central,central_z = 0.0, spectrum=Non
                           % (raw_peak, fit_peak, abs(raw_peak - fit_peak) / raw_peak))
 
                 num_sn_pix = int(round(max(GAUSS_SNR_SIGMA * eli.fit_sigma, GAUSS_SNR_NUM_AA)/pix_size)) #half-width in AA
+                num_sn_pix = int(round(min(num_sn_pix,len(wave_counts)/2 - 1))) #don't go larger than the actual array
 
                 #?rms just under the part of the plot with signal (not the entire fit part) so, maybe just a few AA or pix
                 eli.fit_norm_rmse = rms(wave_counts, rms_wave, cw_pix=getnearpos(wave_x, eli.fit_x0 ), hw_pix=num_sn_pix,
@@ -567,6 +591,8 @@ def signal_score(wavelengths,values,errors,central,central_z = 0.0, spectrum=Non
                              norm=False)
 
                 num_sn_pix = num_sn_pix * 2 + 1 #need full width later (still an integer)
+
+                eli.sn_pix = num_sn_pix
     except Exception as ex:
         if ex.message.find("Optimal parameters not found") > -1:
             log.info("Could not fit gaussian near %f" % central,exc_info=False)
@@ -586,6 +612,8 @@ def signal_score(wavelengths,values,errors,central,central_z = 0.0, spectrum=Non
     else:
         accept_fit = False
         snr = 0.0
+        eli.snr = 0.0
+        eli.line_score = 0.0
         eli.line_flux = 0.0
 
     log.debug("SNR at %0.2f = %0.2f"%(central,snr))
@@ -594,12 +622,12 @@ def signal_score(wavelengths,values,errors,central,central_z = 0.0, spectrum=Non
 
     #todo: re-calibrate to use SNR instead of SBR ??
     sbr = snr
-    #if sbr is None:
-    #    sbr = est_peak_strength(wavelengths,values,central,values_units)
-    #    if sbr is None:
-    #        #done, no reason to continue
-    #        log.warning("Could not determine SBR at wavelength = %f" %central)
-    #        return None
+    if sbr is None:
+        sbr = est_peak_strength(wavelengths,values,central,values_units)
+        if sbr is None:
+            #done, no reason to continue
+            log.warning("Could not determine SBR at wavelength = %f. Will use SNR." %central)
+            sbr = snr
 
     score = sbr
     eli.sbr = sbr
@@ -720,11 +748,11 @@ def signal_score(wavelengths,values,errors,central,central_z = 0.0, spectrum=Non
         #              dx0, rh, error,eli.fit_rmse, si, sk, ku)
 
         title += "%0.2f z_guess=%0.4f A(%d) G(%d)\n" \
-                 "Score = %0.2f , SBR = %0.2f (%0.1f), SNR = %0.2f (%0.1f) wpix = %d\n" \
+                 "Line Score = %0.2f , SBR = %0.2f (%0.1f), SNR = %0.2f (%0.1f) wpix = %d\n" \
                  "Peak = %0.2g, Line(A) = %0.2g, Cont = %0.2g, EqW_Obs=%0.2f\n"\
                  "dX0 = %0.2f, RH = %0.2f, RMS = %0.2f (%0.2f) \n"\
                  "Sigma = %0.2f, Skew = %0.2f, Kurtosis = %0.2f"\
-                  % (eli.fit_x0,central_z,a,g,eli.snr * eli.line_flux * 1.0e17,sbr,
+                  % (eli.fit_x0,central_z,a,g,eli.line_score,sbr,
                      signal_calc_scaled_score(sbr),snr,signal_calc_scaled_score(snr),num_sn_pix,
                      eli.fit_h,eli.line_flux, eli.cont,eli.eqw_obs,
                      dx0, rh, error,eli.fit_rmse, si, sk, ku)