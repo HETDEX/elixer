ff9ba754f07b5a26b2de15a6a174c0c411a0b73c
phantomamoeba <polonius@davistx.com>
2019-05-31 21:48:09 -0500

    1.7.0

--- global_config.py
+++ global_config.py
@@ -20,7 +20,7 @@ PYTHON_MAJOR_VERSION = sys.version_info[0]
 PYTHON_VERSION = sys.version_info
 
 
-HDR1 = False #set to TRUE for HDR1 release
+HDR1 = True #set to TRUE for HDR1 release
 
 if HDR1: #set these paths as appropriate for HETDEX DATA RELEASE-1
     #base path: /work/03946/hetdex/hdr1/
@@ -361,7 +361,7 @@ DYNAMIC_MAG_APERTURE = True  #allow aperture size to change to fit maximum magni
 FIXED_MAG_APERTURE = 1.5 #radius in arcsec
 NUDGE_MAG_APERTURE_CENTER = 1.0  #allow the center of the mag aperture to drift to the 2D Gaussian centroid
                                  #up to this distance in x and y in arcsec (if 0.0 then no drift is allowed)
-MAX_SKY_SUBTRACT_MAG = 5.0 #if local sky subtraction results in a magnitude change greater than this value, do not apply it
+MAX_SKY_SUBTRACT_MAG = 2.0 #if local sky subtraction results in a magnitude change greater than this value, do not apply it
 
 DEBUG_SHOW_GAUSS_PLOTS = False #set on command line now --gaussplots (but keep here for compatibility with other programs)
 
