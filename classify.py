18e75b3d195a310e1aa91a967d9ffc32848ef3a8
phantomamoeba <polonius@davistx.com>
2020-01-07 17:05:35 -0600

    1.8.0a3 - in progress - changed classify::line_finder to solution_finder modified return to be dictionary with lists

--- classify.py
+++ classify.py
@@ -69,23 +69,108 @@ def plae_poii(line_wave,line_flux,line_flux_err, eqw_obs, eqw_obs_err,
 
 
 #todo: ELiXer additional line finder
-def line_finder(waves, flux, flux_err, line_wave=None):
+def solution_finder(waves, flux, flux_err, line_wave=None):
     """
-    Scan spectra and return list of lines that meet minimum criteria (per ELiXer)
+    Scan input spectrum and return dictonary containing extracted info and lists of lines and possible solutions
 
     :param waves: wavelength array in angstroms
-    :param flux: flux
-    :param flux_err: flux_err
+    :param flux: flux array (in cgs e-17 units). These are the default HETDEX spectra units
+                 (flux density x2AA bin)
+    :param flux_err: flux_err array (in cgs e-17 units)
     :param line_wave: optional wavelength of indentified main emission line
-    :return:
+    :return: spectrum dictionary containing:
+                'primary_wave': (float) the observed wavelength of the primary emission line
+                'eqw_obs': (float,float) (observed equivalent width, error)
+                'plae_poii': (float,(float,float)) (PLAE/POII, (mininum PLAE/POII, maximum PLAE/POII))
+                'emission_lines': list of all posssible lines found as dictionaries
+                'solutions': list of all solutions as dictionaries
+
+            'emission_lines' dictionary contains:
+                'wave': (float) observed wavelength,
+                'continuum': (float) observed estimated continuum
+                'fwhm': (float) observed fwhm
+                'int_flux': (float) observed integrated line flux
+                'snr': (float) S/N
+                'line_score': (float) ELiXer line score
+                'aborption': (bool) True if this is an absorption feature
+
+            'solutions' dictionary contains:
+                'wave_rest': (float) rest-frame wavelegth
+                'name':(string) symbolic name,
+                'z': (float) redshift,
+                'score': (float) ELiXer solution score,
+                'frac_score': (float) fractional score = this solution score / sum of all solution scores,
+                'lines': list of lines included in the solution,
+                'rejected_lines': list of found lines removed from the solution (too poor, too close to another line)
+
+            'lines' and 'rejected_lines' dictionaries contain:
+                 'wave_obs': (float) observed wavelength,
+                 'wave_rest': (float) rest wavelength,
+                 'name': (string) sybolic name,
+                 'z': (float) redshift,
+                 'line_score': (float) ELiXer line score,
+                 'snr': (float) S/N
+                 'int_flux':(float) observed integrated line flux
+                 'fwhm': (float) observed fwhm
+                 'aborption': (bool) True if this is an absorption feature
+
     """
 
     spec = elixer_spectrum.Spectrum()
-
-    spec.set_spectra(wavelengths=waves,values=flux,errors=flux_err,central=line_wave)
-
+    spec.set_spectra(wavelengths=waves,values=flux,errors=flux_err,central=line_wave,values_units=-17)
     solutions = spec.classify()
 
-    #todo: maybe return the spectrum object (it has all the solutions and lines)
+    spec_dict = {'primary_wave': spec.central,
+                 'eqw_obs': (spec.eqw_obs, spec.eqw_obs_unc),
+                 'plae_poii': (spec.p_lae_oii_ratio_range[0],
+                               (spec.p_lae_oii_ratio_range[1], spec.p_lae_oii_ratio_range[2])),
+                 'emission_lines': [],
+                 'solutions': []
+                 }
 
-    return solutions
+    if solutions is not None and len(solutions) > 0:
+        for line in spec.all_found_lines:
+            line_dict = {'wave': line.mcmc_x0 if line.mcmc_x0 else line.fit_x0,
+                         'continuum':line.mcmc_continuum if line.mcmc_continuum else line.fit_continuum,
+                         'fwhm':line.fwhm,
+                         'int_flux':line.mcmc_line_flux if line.mcmc_line_flux else line.fit_line_flux,
+                         'snr':line.mcmc_snr if line.mcmc_snr > -1 else line.snr,
+                         'line_score':line.line_score,
+                         'aborption': line.absorber}
+            spec_dict['emission_lines'].append(line_dict)
+
+        for sol in solutions:
+            sol_dict = {'wave_rest':sol.central_rest,
+                        'name':sol.name,
+                        'z':sol.z,
+                        'score': sol.score,
+                        'frac_score':sol.frac_score,
+                        'lines': [],
+                        'rejected_lines': []
+                        }
+            for line in sol.lines:
+                line_dict = {'wave_obs': line.w_obs,
+                             'wave_rest': line.w_rest,
+                             'name': line.name,
+                             'z': line.z,
+                             'line_score': line.line_score,
+                             'snr':line.snr,
+                             'int_flux':line.flux,
+                             'fwhm':line.sigma * 2.355,
+                             'aborption': line.absorber}
+                sol_dict['lines'].append(line_dict)
+            for line in sol.rejected_lines:
+                line_dict = {'wave_obs': line.w_obs,
+                             'wave_rest': line.w_rest,
+                             'name': line.name,
+                             'z': line.z,
+                             'line_score': line.line_score,
+                             'snr':line.snr,
+                             'int_flux':line.flux,
+                             'fwhm':line.sigma * 2.355,
+                             'aborption': line.absorber}
+                sol_dict['rejected_lines'].append(line_dict)
+
+            spec_dict['solutions'].append(sol_dict)
+
+    return spec_dict
