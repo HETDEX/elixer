3f0d80835c87c311606c5fd559cbbb19443f1e59
phantomamoeba <polonius@davistx.com>
2020-07-18 14:09:39 -0500

    1.9.0 -- HDR2.1 (convert image_db_create to SLURM)

--- elixer/image_db_create.sh
+++ elixer/image_db_create.sh
@@ -6,19 +6,44 @@ rpt="elixer_reports_"
 nei="_nei"
 mini="_mini"
 
-echo Running prefixes 0 to 12
-echo This copies databases to staging location.
+#############################################
+# uncommment the next section to run
+# in an idev session:
+# idev -A Hobby-Eberly-Telesco -t 20:00:00
+############################################
 
-read -p "Press enter to continue"
+#echo Running prefixes 0 to 12
+#echo This copies databases to staging location.
+#
+#read -p "Press enter to continue"
+#
+#for i in {21000..21012}
+#do
+#  echo "$db_dir$rpt$i.db"
+#
+#  python3 make_report_db.py --db_name "$db_dir$rpt$i.db" --img_dir $imgdir --img_name "$i*[0-9].png" --mv2dir $cp2dir &
+#
+#  python3 make_report_db.py --db_name "$db_dir$rpt$i$nei.db" --img_dir  $imgdir  --img_name "$i*[0-9]_nei.png" --mv2dir $cp2dir &
+#
+#  python3 make_report_db.py --db_name "$db_dir$rpt$i$mini.db" --img_dir  $imgdir  --img_name "$i*[0-9]_mini.png" --mv2dir $cp2dir &
+#
+#done
+
+#################################
+# -OR-
+#
+# use this section below to create an
+# elixer_reports.run file for the pre-made
+# elixer_reports.slurm file
+####################################
 
 for i in {21000..21012}
 do
-  echo "$db_dir$rpt$i.db"
-  
-  python3 make_report_db.py --db_name "$db_dir$rpt$i.db" --img_dir $imgdir --img_name "$i*[0-9].png" --mv2dir $cp2dir &
-  
-  python3 make_report_db.py --db_name "$db_dir$rpt$i$nei.db" --img_dir  $imgdir  --img_name "$i*[0-9]_nei.png" --mv2dir $cp2dir &
-  
-  python3 make_report_db.py --db_name "$db_dir$rpt$i$mini.db" --img_dir  $imgdir  --img_name "$i*[0-9]_mini.png" --mv2dir $cp2dir &
+
+  echo python3 make_report_db.py --db_name "$db_dir$rpt$i.db" --img_dir $imgdir --img_name "$i*[0-9].png" --mv2dir $cp2dir &
+
+  echo python3 make_report_db.py --db_name "$db_dir$rpt$i$nei.db" --img_dir  $imgdir  --img_name "$i*[0-9]_nei.png" --mv2dir $cp2dir &
+
+  echo python3 make_report_db.py --db_name "$db_dir$rpt$i$mini.db" --img_dir  $imgdir  --img_name "$i*[0-9]_mini.png" --mv2dir $cp2dir &
 
 done
