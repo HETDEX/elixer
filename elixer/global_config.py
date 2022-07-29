03757c0b088c07527f3fabc10e52ba517f6b4f00
phantomamoeba <dustin.davis@utexas.edu>
2022-07-29 08:17:55 -0500

    1.17.0a3 [2022-07-28] --in progress-- test full_fit_scan * Project LineFlux Image to the WCS of the master_cutout (neighborhood) and stacked_cutout (main report) so the NorthBox   matches; Added option (PROJECT_LINE_IMAGE_TO_COMMON_WCS in global_config.py) to turn this off and go back to the   native LineFlux Image projection * Allow hourangle format in --coords file * Auto prepend (9x) and (000 - 999) to manual IDs in --coords file to allow for overlapping shots (if shotid not specified) * Add --require_hetdex optional parameter. If present, will not generated report(s) unless HETDEX data is present * Fix a few chi2 calcs that were using the SNR +/- 2 sigma range rather than the full fitted range * Changed default GAUSSIAN_FIT_AA_RANGE for line finder from 20AA to HETDEX standard 50AA * Fix --aperture bug when specified with a detectID (would except out and fail as was expecting --aperture combined with   --ra and --dec). Will now allow --aperture with --dets and trigger a re-extraction for that detectID. * updated line finders; removed old peakdet with scan for line shape; added (option) to evaluate each wavelength bin   to fit a Gaussian (quick fit); updated curve_fit to attempt different cominations of line widths, flux arrays, etc   to better find weak, normal, and broad lines * updated WD classifier to specify DA or DAB, DB, or DQ and uses line width (sigma) to help distinguish from A stars

--- elixer/global_config.py
+++ elixer/global_config.py
@@ -1033,7 +1033,7 @@ ALLOW_BROADLINE_FIT = True
 
 LINE_FINDER_MEDIAN_SCAN = 5 #SNR scan after applying a median filter to the flux; the value is the #of pixels, must be odd
                             #set to 0 to turn off
-LINE_FINDER_FULL_FIT_SCAN = False #scan at each pixel (each bin in CALFIB_WAVEGRID) and try to fit emission and/or abosrption
+LINE_FINDER_FULL_FIT_SCAN = True #scan at each pixel (each bin in CALFIB_WAVEGRID) and try to fit emission and/or abosrption
                             #NOTE: this will still run if False when no lines are found with other methods
 
 SUBTRACT_HETDEX_SKY_RESIDUAL = False #if true compute a per-shot sky residual, convolve with per-shot PSF and subtract
