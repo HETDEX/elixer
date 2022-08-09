e0429077a8d4d0966ae90d79cc621f23c9e56c85
phantomamoeba <dustin.davis@utexas.edu>
2022-08-09 08:46:29 -0500

    1.17.0a4 [2022-08-03] -- slice off just most extreme 16% from either end * replace fixed HETDED gmag limit (was 25.0) with computed limit from the IFU + shot for each detection (using mean   of "empty" fibers as a uniform background in a 3.5" radius aperture) * renamed best_z and best_pz in h5 file to z_best and z_best_pz * added mag_g_wide_limit to h5 file as the computed HETDEX gmag limit (or 25.0 by default)

--- elixer/spectrum_utilities.py
+++ elixer/spectrum_utilities.py
@@ -253,7 +253,7 @@ def calc_dex_g_limit(calfib,calfibe=None,fwhm=1.7,flux_limit=4.5,wavelength=4640
             pass
         elif True: #this gives the "deepest" results #~ 24.9 +/- 0.35 with some pushing 26
             sz = len(all_calfib)
-            trim_frac =  0.10 #maybe a larger range??
+            trim_frac =  0.16 #maybe a larger range??
             sel = [x for _, x in sorted(zip(cont_calfib, np.arange(sz)))][int(trim_frac * sz):int(-1 * trim_frac * sz)]
             all_calfib = all_calfib[sel]
             all_calfibe = all_calfibe[sel]
