c8901471257c106278d5759cbe7fee080c89b865
phantomamoeba <dustin.davis@utexas.edu>
2022-11-30 16:16:54 -0600

    1.18.0

--- elixer/random_apertures/merge_tables.py
+++ elixer/random_apertures/merge_tables.py
@@ -6,9 +6,13 @@ table_outname = "random_apertures_"
 
 files = glob.glob(table_outname +"*.fits")
 T = Table.read(files[0],format="fits")
+write_every = 100
 for i,f in enumerate(files[1:]):
     print(i+1,f)
     t = Table.read(f,format="fits")
     T = vstack([T,t])
+
+    if (i+1) % write_every == 0:
+        T.write(table_outname+"_all.fits",format='fits',overwrite=True)
 
 T.write(table_outname+"_all.fits",format='fits',overwrite=True)
