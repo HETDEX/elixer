1.18.2a2 --test-- P(LyA) threshold back to 0.5 * fix bug in spectrum::solution_consistent_with_low_z(); was using wrong index and could mark lines as missing when they   are found * add special handling for test against bright OIII-5007 * slight tuning of required line weights for Balmer series * testing Flam slope vote .. for g < 23.5, fwhm < 1200 km/s, with negative slope < -4e-22 flux/AA and wave > 3950 * testing EW + PLAE correction vote * fixed missing threshold conversion on P(LyA)

elixer/hetdex.py
@@ -5205,8 +5205,8 @@ class DetObj:
             try:
                 if len(weight) > 0 and np.sum(weight) < 1.0:
                     tot_weight = np.sum(weight)
-                    log.info(f"Low voting weight ({tot_weight}). Adding in 0.5 vote at {1.0-tot_weight} weight.")
-                    likelihood.append(0.5)
+                    log.info(f"Low voting weight ({tot_weight}). Adding in {G.PLYA_VOTE_THRESH} vote at {1.0-tot_weight} weight.")
+                    likelihood.append(G.PLYA_VOTE_THRESH)
                     weight.append(1.0 -tot_weight)
                     var.append(1.0)
                     prior.append(0.5)