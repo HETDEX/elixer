150e95730c4e37daa7a1aa75a76eb13509622c38
phantomamoeba <dustin.davis@utexas.edu>
2023-04-19 12:03:52 -0500

    1.19.0a13 --FF Sky model update to ra06, increase LAE Random Apertures to search out to 7.0" in steps of 0.5" (was limited to 3.0" previously)  --turn ON model sky residual subtraction, Model_ra06 (based on random apertures rather than fibers) ... use stack vs poly6 model  --turn ON random apertures near central detection objects * bumped max distance to curve for HETDEX match to imaging from 0.5" (0.75" for early data) to 1.25" (and 1.75") * added interpolated, per wavelength bin modeled PSF neighbor deblending * fixed skip of line fitting for elixer checked incompatible conditions when --fit_sigma is used

--- elixer/elixer.py
+++ elixer/elixer.py
@@ -5611,7 +5611,7 @@ def main():
 
                         return d
 
-                    aperture_displacement_list = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0])/3600.
+                    aperture_displacement_list = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0,5.5,6.0,6.5,7.0])/3600.
                     sep_dist_min = 0.5
                     #do this for each object we have
                     for hd in hd_list:
