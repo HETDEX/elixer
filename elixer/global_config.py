4c951538b96180f204ee4d4071a675e951f21d55
phantomamoeba <dustin.davis@utexas.edu>
2023-06-13 10:24:29 -0500

    #turn back ON per-fiber zero point correction and add first test of FF sky 1.20.0a4 [2023-06-12] [in progress] **testing UVBackground correction**
    * added check for brightest single fiber being massively brighter than the next brightest (as weighted). This is an
      indication of a problem.
    * added check for possible charge trap based on central column(s) of sky-subtracted (cleaned) image cutout
    * fixed bug in cat_cfhtls, goods_n and egs that was skipping cutout collection when the catalog matches were zero
    * update decals call to use DR10
    * add waring to stdout, elixer.log, and to PDF report if --continuum flag not used with a continuum source or is
      used with an emission line source
    * added extra check for blank region in PNG conversion where the 2D fiber cutouts should be (this is usually a spot
      where a PDF to PNG conversion can fail silently). Will retry conversion if that region is blank.
    * fiber to PSF correction for 3.5" apertures and seeing FWHM 1.0 to 3.0 interpolated based on pre-computed curves

--- elixer/global_config.py
+++ elixer/global_config.py
@@ -1306,7 +1306,7 @@ VOTE_EW_PLAE_POII_CORRECTION    = 0x00000800
 # testing sky residuals
 ###################################
 
-APPLY_SKY_RESIDUAL_TYPE = 0 #0 = No, off, do not use:   1 = per fiber, 2 = per 3.5" aperture
+APPLY_SKY_RESIDUAL_TYPE = 1 #0 = No, off, do not use:   1 = per fiber, 2 = per 3.5" aperture
 #Note: there are many issues in ELiXer and post-ELiXer for the aperture version, so it has been removed
 
 SKY_RESIDUAL_PER_SHOT = False #if True pull each residusl from the match shot, if False, use the universal model
@@ -1331,7 +1331,7 @@ SKY_RESIDUAL_FITS_COL  = None #"ll_stack_050"
 SKY_RESIDUAL_ALL_LL_MODELS = None
 SKY_RESIDUAL_HDR3_ALL_LL_MODELS_FN = op.join(op.dirname(op.realpath(__file__)), "sky_subtraction_residuals/hdr3_local_sky_fiber_residual_models_by_psf.txt")
 SKY_RESIDUAL_ALL_FF_MODELS = None
-SKY_RESIDUAL_HDR3_ALL_FF_MODELS_FN = None# op.join(op.dirname(op.realpath(__file__)), "sky_subtraction_residuals/hdr3_ff_sky_fiber_residual_model.txt")
+SKY_RESIDUAL_HDR3_ALL_FF_MODELS_FN = op.join(op.dirname(op.realpath(__file__)), "sky_subtraction_residuals/hdr3_ff_sky_fiber_residual_models_by_psf.txt")
 SKY_RESIDUAL_ALL_PSF = np.arange(1.2,3.1,0.1)
 
 
