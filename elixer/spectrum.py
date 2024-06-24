1.22.0a14 [2024-06-24] * if an SEP object is removed from consideration for the combined_continuum calculation, also remove it from   consideration in the angular extent calculation * fixed issue where web-based imaging were not being checked if a match was found to at least one local image but   that local image was bad or empty * changed EW + PLAE/POII correction vote to use dynamic midpoint (based on wavelength) rather than fixed 1.0 value;   switched to use the plae_poii_hat value rather than the minimum value to trigger   + allow also OII correction * increased max OII Gaussian fit sigma from 6.5 to 7.5 based on rare high width OII. Transition threshold stays at 4.5.   Switch to "sigmoid_linear_interp" ... kind of like a sigmoid in that the values are constant (or nearly constant)   away from the transition, but here the transition zone is linear. * turn down weight of asymmetric line flux vote * update EW straight vote to use sigmoid_linear_interp() and slightly tuned weighting * PLAE/POII high z minimum threshold reduced  from 10 to 8 * require OII-3727 if Balmer lines found for low-z galaxy consistency

elixer/spectrum.py
@@ -5945,13 +5945,17 @@ class Spectrum:
             # 1 for youself along the diagnonal
             # 1 also for a line that probably should be found, but is not catastrophic if not found
             # > 1 for lines that really must be found
+            # !!! note: not strictly true that we must have OII-3727 if we see the Balmer lines,
+            #     but for HETDEX data and the majority of late (low-z) galaxies this is so
+            #     (not likely to see some pristine, supper young galaxy without detectable OII ....
+            #      though there have been a few weird exampele where HETDEX picks up OIII-5007 but strangely not OII-3727)
             match_matrix =[[1,0,0,0,0,0,0,0,0],  #0 [OII]
                            [1,1,1,1,2,3,5,0,0],  #1 H_eta
                            [1,0,1,1,2,3,5,0,0],  #2 H_zeta
                            [1,0,0,1,1,2,5,0,0],  #3 H_epsilon
                            [1,0,0,0,1,1,5,0,0],  #4 H_delta
-                           [0,0,0,0,0,1,5,0,0],  #5 H_gamma
-                           [0,0,0,0,0,0,1,0,0],  #6 H_beta
+                           [1,0,0,0,0,1,5,0,0],  #5 H_gamma
+                           [1,0,0,0,0,0,1,0,0],  #6 H_beta
                            [1,0,0,0,0,0,0,1,2],  #7 OIII 4959
                            [1,0,0,0,0,0,0,0,1]]  #8 OIII 5007
             match_matrix = np.array(match_matrix)