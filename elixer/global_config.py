ba38c98a49d06fe874c9b0323d8be0d365cac57b
phantomamoeba <dustin.davis@utexas.edu>
2024-06-25 08:25:23 -0500

    1.22.0a14 [2024-06-24] --experiment with piecewise linear PLAE/POII  v11 4000,5000,1.4, 8.0-- * if an SEP object is removed from consideration for the combined_continuum calculation, also remove it from   consideration in the angular extent calculation * fixed issue where web-based imaging were not being checked if a match was found to at least one local image but   that local image was bad or empty * changed EW + PLAE/POII correction vote to use dynamic midpoint (based on wavelength) rather than fixed 1.0 value;   switched to use the plae_poii_hat value rather than the minimum value to trigger   + allow also OII correction * increased max OII Gaussian fit sigma from 6.5 to 7.5 based on rare high width OII. Transition threshold stays at 4.5.   Switch to "sigmoid_linear_interp" ... kind of like a sigmoid in that the values are constant (or nearly constant)   away from the transition, but here the transition zone is linear. * turn down weight of asymmetric line flux vote * update EW straight vote to use sigmoid_linear_interp() and slightly tuned weighting * require OII-3727 if Balmer lines found for low-z galaxy consistency

--- elixer/global_config.py
+++ elixer/global_config.py
@@ -1228,9 +1228,10 @@ MAX_MAG_FAINT = 28.0 #set as nominal "faint" mag if flux limit reached (if not s
                      # this is an HST catalog, this is pretty good (HST is 29-30)
 
 PLAE_POII_GAUSSIAN_WEIGHT_SIGMA = 5.0 #5.0 #10.0 s|t by sigma or 1/sigma you get to 80% weight
-PLAE_POII_TRANS_WAVES = [4000.0,4500.0,5000.0,5500.] #must be strictly increasing in wavelength, but can be any number of elements
-PLAE_POII_TRANS_THRESH = [1.4,8.0,10.0,8.0] #favors LyA for larger threholds at or above the matching wavelength
-
+# PLAE_POII_TRANS_WAVES = [4000.0,4500.0,5000.0,5500.] #must be strictly increasing in wavelength, but can be any number of elements
+# PLAE_POII_TRANS_THRESH = [1.4,8.0,10.0,8.0] #favors LyA for larger threholds at or above the matching wavelength
+PLAE_POII_TRANS_WAVES = [4000.0,5000.0] #must be strictly increasing in wavelength, but can be any number of elements
+PLAE_POII_TRANS_THRESH = [1.4,8.0] #favors LyA for larger threholds at or above the matching wavelength
 
 CHECK_FOR_METEOR = True #if true, check the exposure fiber data for meteor pattern
 CHECK_GALAXY_MASK = True #if true, check for detection inclusion in galaxy mask
