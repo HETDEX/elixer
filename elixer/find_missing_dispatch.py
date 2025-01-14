5a6845adfcb8f00f5db5cefd7276bd0d431c93f7
phantomamoeba <dustin.davis@utexas.edu>
2025-01-13 21:38:27 -0600

    1.22.4a1 [2025-01-13] update find_missing_dispatch with tqdm and correction for local pathing naming issue

--- elixer/find_missing_dispatch.py
+++ elixer/find_missing_dispatch.py
@@ -7,8 +7,10 @@ find the dispatch_xxxx that are incomplete from their list of detections
 import glob
 import os
 import numpy as np
+from tqdm import tqdm
 
 workdir = "./"
+
 
 print_missing_dets = False
 
@@ -18,7 +20,7 @@ basename = os.path.basename(os.getcwd())
 
 missing_dispatches = []
 
-for dispatch in dispatches:
+for dispatch in tqdm(dispatches):
     detectids = np.loadtxt(os.path.join(dispatch,os.path.basename(dispatch)),dtype=int)
     detectids = detectids[::-1] #most likely to be the one at the end
 
@@ -30,12 +32,18 @@ for dispatch in dispatches:
             if print_missing_dets:
               print(detectid)
               if detectid not in missing_dispatches:
-                missing_dispatches.append(dispatch)
+                  missing_dispatches.append(os.path.basename(dispatch))
 
             else:
               print(dispatch)
-              missing_dispatches.append(dispatch)
+              missing_dispatches.append(os.path.basename(dispatch))
               break
+
+
+if len(missing_dispatches) > 0:
+    print(f"Incomplete Dispatches: ")
+    for md in missing_dispatches:
+        print(f"{md}")
 
 #now update the elixer.run
 _,run_dispatches = np.loadtxt(os.path.join(workdir,"elixer.run"),dtype=str,usecols=[0,1],unpack=True)
@@ -48,8 +56,13 @@ with open(os.path.join(workdir,"elixer.run"),"r") as f:
 
 _,_, idx = np.intersect1d(missing_dispatches,run_dispatches,return_indices=True)
 
+print(f"Found {len(idx)} matching task. Writing out sweep file ...")
+
 #print(missing_dispatches)
 with open("elixer_sweep.run","w+") as f:
     for i in idx:
-        f.write(' '.join(tasks[i])+"\n")
+        #f.write(' '.join(tasks[i])+"\n")
+        #print(' '.join(tasks[i])+"\n")
+        f.write(tasks[i])
+        #print(tasks[i])
 
