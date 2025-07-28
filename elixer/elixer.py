ad20a4a04ed032430d9a497190d90a2e9717472d
phantomamoeba <dustin.davis@utexas.edu>
2025-07-28 15:09:51 -0500

    1.23.0a1 - working on single shot, parallel science frames            adding in forced (re)extractions

--- elixer/elixer.py
+++ elixer/elixer.py
@@ -1000,10 +1000,12 @@ def parse_commandline(auto_force=False):
     if args.hdf5 is not None:
         #path could have changed
         if os.path.exists(os.path.basename(args.hdf5)):
-            G.HDF5_DETECT_FN = os.path.abspath(args.hdf5)
+            args.hdf5 = os.path.abspath(args.hdf5)
+            G.HDF5_DETECT_FN = args.hdf5
             G.HDF5_CONTINUUM_FN = G.HDF5_DETECT_FN
         elif os.path.exists(os.path.join("../",os.path.basename(args.hdf5))):
-            G.HDF5_DETECT_FN = os.path.abspath(os.path.join("../",os.path.basename(args.hdf5)))
+            args.hdf5 = os.path.abspath(os.path.join("../",os.path.basename(args.hdf5)))
+            G.HDF5_DETECT_FN = args.hdf5
             G.HDF5_CONTINUUM_FN = args.hdf5
         else: #try as is
             G.HDF5_DETECT_FN = args.hdf5
