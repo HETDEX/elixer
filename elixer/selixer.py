9403fa257f0f16205cd3b1cd1d6c57779debd919
phantomamoeba <dustin.davis@utexas.edu>
2025-12-21 16:09:49 -0600

    1.24.1a4 [2025-12-10] --add "=" for changed SLURM parser for --dependency * update restricted catalog cat_rstr_cosmos_hst_jwst for new imaging tiles * add enforceable maglimit on source extractor detection (see global_config SOURCE_EXTRACTOR_MIN_MAG_LIMIT and   the individual catalog mag_limits). Currently only set to potentially trigger for HST and JWST.

--- elixer/selixer.py
+++ elixer/selixer.py
@@ -1619,8 +1619,8 @@ else:
         os.system('sbatch elixer_merge.slurm')
     elif slurm_id_dependency is not None:
         if "after" in slurm_id_dependency:
-            os.system(f'sbatch --dependency {slurm_id_dependency} elixer.slurm')
+            os.system(f'sbatch --dependency={slurm_id_dependency} elixer.slurm')
         else:
-            os.system(f'sbatch --dependency afterany:{slurm_id_dependency} elixer.slurm')
+            os.system(f'sbatch --dependency=afterany:{slurm_id_dependency} elixer.slurm')
     else:
         os.system('sbatch elixer.slurm')
