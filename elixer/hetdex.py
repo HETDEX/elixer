5f497500fa6805120bad49dbf8310e7dc6ce70ec
phantomamoeba <dustin.davis@utexas.edu>
2026-08-26 10:23:23 -0500

    1.26.1a0 [2026-08-26] initial testing * adding Erin's random forest classifier (p_conf)

--- elixer/hetdex.py
+++ elixer/hetdex.py
@@ -14684,13 +14684,15 @@ class HETDEX:
                             #normally this runs on an ARRAY of data, but here we have only one row
                             d.rf_conf_score = RF_CONF.predict_proba([[d.w , d.snr, d.chi2, d.fwhm/2.355,
                                                                       d.cont_cgs_narrow * 2.]])[:, 1]
+                            log.info(f"Random Forest classifier confidence = {d.rf_conf_score} for [{d.w},"
+                                 f" {d.snr}, {d.chi2},"
+                                 f" {d.fwhm / 2.355}, {d.cont_cgs_narrow * 2.}]")
+
                             log.info(f"Random Forest classifier confidence = {d.rf_conf_score:0.2f} for [{d.w:0.2f},"
                                      f" {d.snr:0.2f}, {d.chi2:0.2f},"
                                      f" {d.fwhm/2.355:0.2f}, {float(d.cont_cgs_narrow)*2.:0.4f}]")
 
-                            # log.info(f"Random Forest classifier confidence = {d.rf_conf_score} for [{d.w},"
-                            #      f" {d.snr}, {d.chi2},"
-                            #      f" {d.fwhm / 2.355}, {d.cont_cgs_narrow}]")
+
 
                     except:
                         log.info("Failed to produce pConf scoring", exc_info=True)
